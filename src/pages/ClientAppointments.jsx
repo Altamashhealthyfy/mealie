1b246709c9463a2cc693fe446183954b3610a2f6
sanjeev mishra <iitiansanjeev@gmail.com>
2026-01-10 11:41:44 +0000

    File changes

--- src/pages/ClientAppointments.jsx
+++ src/pages/ClientAppointments.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { base44 } from "@/api/base44Client";
 import { useQuery } from "@tanstack/react-query";
-import PageTour from "@/components/common/PageTour";
-import TourButton from "@/components/common/TourButton";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
