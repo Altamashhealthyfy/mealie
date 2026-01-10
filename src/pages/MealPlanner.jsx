eb5dadb8a346890138c685b1c3c26a7746fea3c1
sanjeev mishra <iitiansanjeev@gmail.com>
2026-01-10 11:37:01 +0000

    File changes

--- src/pages/MealPlanner.jsx
+++ src/pages/MealPlanner.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import PageTour from "@/components/common/PageTour";
-import TourButton from "@/components/common/TourButton";
+
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
